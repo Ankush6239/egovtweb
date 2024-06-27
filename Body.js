b27a615fcb36874d9c2a474474bc525499bcc57f
Ankush6239 <154660628+Ankush6239@users.noreply.github.com>
2024-06-28 01:00:45 +0530

    Add files via upload

--- Body.js
+++ Body.js
@@ -9,9 +9,10 @@ function Body() {
             <div className="card-group mt-1">
               <div className="card">
                 <img
-                  src="https://w7.pngwing.com/pngs/255/570/png-transparent-computer-icons-university-university-bulding-angle-rectangle-university-thumbnail.png"
+                  src="https://img.icons8.com/?size=100&id=3496&format=png&color=000000"
                   className="card-img-top"
-                  alt="..." id="img"
+                  alt="..."
+                  id="img"
                 />
                 <div className="card-body">
                   <h5 className="card-title">Your Government</h5>
@@ -19,54 +20,96 @@ function Body() {
               </div>
               <div className="card">
                 <img
-                  src="https://www.freepik.com/icon/suitcase_16010357#fromView=search&page=1&position=4&uuid=4f81e6fa-b519-4265-a6eb-b1759ba22a0c"
-                  className="card-img-top"
-                  alt="..." id="img"
+                  src="https://img.icons8.com/?size=100&id=21817&format=png&color=000000"
+                  alt=""
+                  id="img"
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Your Government</h5>
+                  <h5 className="card-title">
+                    Jobs and <br />
+                    Unemployment
+                  </h5>
                 </div>
               </div>
               <div className="card">
                 <img
-                  src="https://w7.pngwing.com/pngs/255/570/png-transparent-computer-icons-university-university-bulding-angle-rectangle-university-thumbnail.png"
+                  src="https://img.icons8.com/?size=100&id=EhK5kQGo9qdp&format=png&color=000000"
                   className="card-img-top"
-                  alt="..." id="img"
+                  alt="..."
+                  id="img"
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Your Government</h5>
+                  <h5 className="card-title">
+                    Business and <br />
+                    Industry
+                  </h5>
                 </div>
               </div>
               <div className="card">
                 <img
-                  src="https://w7.pngwing.com/pngs/255/570/png-transparent-computer-icons-university-university-bulding-angle-rectangle-university-thumbnail.png"
+                  src="https://img.icons8.com/?size=100&id=241&format=png&color=000000"
                   className="card-img-top"
-                  alt="..." id="img"
+                  alt="..."
+                  id="img"
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Your Government</h5>
+                  <h5 className="card-title">
+                    Roads and <br />
+                    Transport
+                  </h5>
                 </div>
               </div>
               <div className="card">
                 <img
-                  src="https://w7.pngwing.com/pngs/255/570/png-transparent-computer-icons-university-university-bulding-angle-rectangle-university-thumbnail.png"
+                  src="https://img.icons8.com/?size=100&id=2787&format=png&color=000000"
                   className="card-img-top"
-                  alt="..." id="img"
+                  alt="..."
+                  id="img"
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Your Government</h5>
+                  <h5 className="card-title">
+                    Culture and <br />
+                    Recreation
+                  </h5>
                 </div>
               </div>
               <div className="card">
                 <img
-                  src="https://w7.pngwing.com/pngs/255/570/png-transparent-computer-icons-university-university-bulding-angle-rectangle-university-thumbnail.png"
+                  src="https://img.icons8.com/?size=100&id=2112&format=png&color=000000"
                   className="card-img-top"
-                  alt="..." id="img"
+                  alt="..."
+                  id="img"
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Your Government</h5>
+                  <h5 className="card-title">
+                    Justice, Safty and <br />
+                    the law
+                  </h5>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p id="p">
+            The official guide to living, working, visiting and investing in the
+            Texas
+            <div className="btn btn-primary" id="btn">
+              Let’s explore more
+            </div>
+          </p>
+        </div>
+      </div>
+      <div className="container bg-light-dark">
+        <div className="row">
+          <div className="col-xl-6" id="xl">
+            <h2 id="h2">
+              Let’s explore local services, programs & initiatives.{" "}
+            </h2>
+          </div>
+          <div className="col-xl-6  justify-content-end" id="xl6">
+            <div className="btn btn-primary " id="btn2">
+              Explore Services
             </div>
           </div>
         </div>
---
+++ Homecard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Homecard(props) {
+  return (
+    <div>
+      <div className="container">
+      <div class="card" style="width: 18rem;">
+  <img src={props.url} class="card-img-top" alt="...">
+</div>
+<div></div>
+      </div>
+    </div>
+  )
+}
